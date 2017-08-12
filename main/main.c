763539213a8b1610d01142a1950a09d37c8cbcdb
johanvdp <johanvdp@hotmail.com>
2017-08-13 01:01:39 +0200

    Make blink listen to configuration

--- main/main.c
+++ main/main.c
@@ -1,11 +1,4 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+// The author disclaims copyright to this source code.
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -18,22 +11,19 @@
 
 void blink_task(void *pvParameter)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
+	// init
     gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    // loop forever
     while(1) {
-        /* Blink off (output low) */
+        /* OFF */
         gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1950 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
+        vTaskDelay(BLINK_OFF_MS / portTICK_PERIOD_MS);
+
+        /* ON */
         gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(50 / portTICK_PERIOD_MS);
+        vTaskDelay(BLINK_ON_MS / portTICK_PERIOD_MS);
     }
 }
 
