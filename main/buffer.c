6a40390374190f38bbfc69911397fd8e6b83206f
johanvdp <johanvdp@hotmail.com>
2017-08-26 18:17:52 +0200

    Add  first version ring buffer

---
+++ main/buffer.c
@@ -0,0 +1,51 @@
+// The author disclaims copyright to this source code.
+#include "buffer.h"
+
+static const char* TAG = "buffer.c";
+
+/**
+ * Buffer algorithm only works when memory size is a power of two.
+ */
+#define BUFFER_MASK (MEM_TOTAL_BYTES - 1)
+
+uint32_t buffer_read_addr;
+uint32_t buffer_write_addr;
+
+uint32_t buffer_available() {
+	return buffer_write_addr - buffer_read_addr;
+}
+
+uint32_t buffer_free() {
+	return MEM_TOTAL_BYTES - (buffer_write_addr - buffer_read_addr);
+}
+
+void buffer_push(uint8_t *data, uint32_t length) {
+	assert(buffer_free() >= length);
+	mem_data_write(buffer_write_addr, length, data);
+	buffer_write_addr = (buffer_write_addr + length) & BUFFER_MASK;
+}
+
+void buffer_pull(uint8_t *page, uint32_t length) {
+	assert(buffer_available() >= length);
+	mem_data_read(buffer_read_addr, length, page);
+	buffer_read_addr = (buffer_read_addr + length) & BUFFER_MASK;
+}
+
+void buffer_log_configuration() {
+	ESP_LOGD(TAG, ">buffer_log_configuration");
+	//ESP_LOGI(TAG, "CONFIG_: %d", CONFIG_);
+	ESP_LOGD(TAG, "<buffer_log_configuration");
+}
+
+void buffer_initialize() {
+	ESP_LOGD(TAG, ">buffer_initialize");
+	buffer_read_addr = 0;
+	buffer_write_addr = 0;
+	mem_begin_command((spi_host_device_t) VSPI_HOST);
+	mem_command_write_mode_register(MEM_MODE_SEQUENTIAL);
+	mem_command_read_mode_register();
+	mem_end();
+	mem_begin_data((spi_host_device_t) VSPI_HOST);
+	ESP_LOGD(TAG, "<buffer_initialize");
+}
+
---
+++ main/buffer.h
@@ -0,0 +1,55 @@
+// The author disclaims copyright to this source code.
+#ifndef _BUFFER_H_
+#define _BUFFER_H_
+
+/**
+ * @file
+ * Ring buffer.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_heap_caps.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "mem.h"
+
+/**
+ * Check number of bytes that can be pulled from the buffer.
+ * @return Number of bytes.
+ */
+uint32_t buffer_available();
+/**
+ * Check number of bytes that can be pushed into the buffer.
+ * @return Number of bytes.
+ */
+uint32_t buffer_free();
+/**
+ * Push a number of bytes into the buffer.
+ * @param data Source of data.
+ * @param length Number of bytes.
+ */
+void buffer_push(uint8_t *data, uint32_t length);
+/**
+ * Pull a number of bytes from the buffer.
+ * @param data Target of data.
+ * @param length Number of bytes.
+ */
+void buffer_pull(uint8_t *page, uint32_t length);
+/**
+ * Log buffer configuration.
+ */
+void buffer_log_configuration();
+/**
+ * Initialize buffer once before operation.
+ */
+void buffer_initialize();
+
+#endif
